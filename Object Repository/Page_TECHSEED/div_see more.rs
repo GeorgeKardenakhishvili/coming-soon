8daf50dc932bd7ddf1c459b7fdbf131cf5ef01e7
GiorgiKardenaxishvili <kardenaxishvilig@gmail.com>
2023-04-25 13:09:07 +0400

    katalon

---
+++ Object Repository/Page_TECHSEED/div_see more.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_see more</name>
+   <tag></tag>
+   <elementGuidId>f7f5c848-a2aa-472c-85e8-4beaf3ba0940</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/div/div[2]/div/div/div[4]/div[2]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.Main_seeMore__WLLev</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>6330009c-59ce-4d30-8f6d-e05b35fa6e18</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>Main_seeMore__WLLev</value>
+      <webElementGuid>363af23a-84e3-4015-867e-9bf0ca29acb8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>see more </value>
+      <webElementGuid>ebe0c916-5a6e-4989-8e70-e0eb0f9da285</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[@class=&quot;App_App__TFwhC&quot;]/div[@class=&quot;App_router__m+lQE&quot;]/div[@class=&quot;Paper_Paper__5bnGs Paper_LightPaper__v5qU6&quot;]/div[@class=&quot;Main_main__OsPI8 Main_Light__GTZg9&quot;]/div[@class=&quot;Main_wwa__qtVUD&quot;]/div[@class=&quot;Main_seeMore__WLLev&quot;]</value>
+      <webElementGuid>26bc62ae-2db0-4480-b814-2e22a20e4975</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div/div[2]/div/div/div[4]/div[2]</value>
+      <webElementGuid>8552a47b-9460-4429-aa11-cf3ef0f20fbf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='are'])[1]/following::div[1]</value>
+      <webElementGuid>dae68f04-52b5-401c-ae6c-de81a15405be</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='FUTURE-PROOF YOURCYBERSECURITYFORTIFY YOUR DIGITALECOSYSTEM'])[1]/following::div[7]</value>
+      <webElementGuid>0b4a0e79-38ec-4b42-a4f7-4190b79ac897</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='about us'])[1]/preceding::div[1]</value>
+      <webElementGuid>b3e52613-898f-4ea8-afab-fb7c9cc903cf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='our mission'])[1]/preceding::div[1]</value>
+      <webElementGuid>f67e0c2e-72f6-4e87-8f63-181c94b37a4f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[4]/div[2]</value>
+      <webElementGuid>f653884c-ff88-4011-9ddd-6ccc4e9c5c78</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'see more ' or . = 'see more ')]</value>
+      <webElementGuid>18373e45-0a8b-444d-83ce-14b5b5c1780d</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
